f19faeaa531f47bc11f2ed6ef4426845047f8163
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-27 20:21:04 -0400

    Fix footer headings

--- src/lib/components/footer/FooterMenu.svelte
+++ src/lib/components/footer/FooterMenu.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="text-white text-center sm:text-left space-y-6">
-  <h4 class="text-lg font-bold">{title}</h4>
+  <h3 class="text-lg font-bold">{title}</h3>
   <ul class="leading-none space-y-4 text-neutral-200">
     {#each links as { href, label } (label)}
       <li><a {href} class="hover:text-primary transition-colors duration-300">{label}</a></li>
